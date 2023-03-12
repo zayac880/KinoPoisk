611d633016b4c737fdbfa5f6003309d82b3733d2
Alex <zayac880880@mail.ru>
2023-03-12 15:48:22 +0300

    1

--- project/views/auth/user.py
+++ project/views/auth/user.py
@@ -1,58 +1,21 @@
 from flask_restx import Namespace, Resource
 
 from project.container import user_service
-from project.setup.api.models import user
+from project.models import User
 
-api = Namespace('users')
+api = Namespace('user')
 
 
 @api.route('/')
 class UserView(Resource):
-    @api.expect(404, 'Not Found')
-    @api.marshal_with(user, as_list=True, code=200, description='OK')
     def get(self):
-        """
-        Получить всех пользователей.
-        """
-        return user_service.get_all()
+        pass
 
-    @api.expect(user)
-    @api.response(201, 'Created')
-    def post(self):
-        """
-        Создать нового пользователя.
-        """
-        new_user = user_service.create(api.payload)
-        return new_user, 201
+    def patch(self):
+        pass
 
 
-@api.route('/<int:user_id>/')
+@api.route('/password')
 class UserView(Resource):
-    @api.response(404, 'Not Found')
-    @api.marshal_with(user, code=200, description='OK')
-    def get(self, user_id: int):
-        """
-        Получить пользователя по идентификатору.
-        """
-        return user_service.get_one(user_id)
-
-    @api.expect(user)
-    @api.response(204, 'No Content')
-    def put(self, user_id: int):
-        """
-        Обновить пользователя по идентификатору.
-        """
-        user_service.update(user_id, api.payload)
-        return None, 204
-
-    @api.response(204, 'No Content')
-    def delete(self, user_id: int):
-        """
-        Удалить пользователя по идентификатору.
-        """
-        user_service.delete(user_id)
-        return None, 204
-
-
-
-
+    def put(self):
+        pass
